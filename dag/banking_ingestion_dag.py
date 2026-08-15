f7db2f377a3391a0b34b161afdd9fe4a69c7e096
Jagdish <jagadishs1997@gmail.com>
2026-08-16 00:11:34 +0530

    Added Airflow code

--- dag/banking_ingestion_dag.py
+++ dag/banking_ingestion_dag.py
@@ -1,1 +1,42 @@
-import airflow
+from airflow import DAG
+from airflow.providers.standard.operators.bash import BaseOperator
+
+from datetime import datetime
+
+PROJECT_HOME = "/home/jdshelke/projects/banking-transaction-monitoring"
+INGESTION_JOB = f"{PROJECT_HOME}/src/jobs/ingestion_job.py"
+
+SOURCE_TABLES = [
+    "branches",
+    "employees",
+    "customers",
+    "accounts",
+    "cards",
+    "loans",
+    "transactions",
+    "card_transactions",
+    "loan_payments",
+    "support_tickets",
+]
+
+with DAG(
+    dag_id="banking_ingestion",
+    start_date=datetime(2026, 8, 15),
+    schedule=None,
+    catchup=False,
+    tags=["banking", "ingestion", "bronze"]
+) as dag:
+
+    for table in SOURCE_TABLES:
+
+        BaseOperator(
+            task_id=f"ingest_{table}",
+            bash_command=(
+                f"spark-submit "
+                f"--master yarn "
+                f"--deploy-mode client "
+                f"--name Banking-Ingestion-{table} "
+                f"{INGESTION_JOB} "
+                f"--table {table}"
+            ),
+        )
